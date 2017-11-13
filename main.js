ff2ed762ec27ee4fe665d5fe75b1ef8e7d92cdca
Kristy Lin <kristyyl@uci.edu>
2017-11-13 14:40:24 -0800

    add javascript buttons/toggle
    added button to "learn more about me" and display older events in extracurricular section.

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
-$(document).ready(function()
-{
-	window.scroll({
-		behavior: 'smooth';
+function animate_nav(linkid, destid){
+    $('#'+linkid).click(function() {
+	    $('html').animate({
+	        scrollTop: $('#'+destid).offset().top
+	    }, '1600');
 	});
+}
 
-	$('#test').click(function(
-    	$('.hidden.img').toggle();
-	));
+function fade_page_load(){
+	$('body').fadeIn('slow');
+}
+
+function toggle_extras(){
+    $('#old-extra').click(function() {
+	    $('#div-hide').slideToggle('slow');
+	   if ($('#old-extra').text() == "Display Older Events"){
+	   		$('#old-extra').text("Collapse Older Events");
+	   }
+	   else{
+	   		$('#old-extra').text("Display Older Events")
+	   }
+	});
+}
+
+
+$(document).ready(function(){
+
+	fade_page_load();
+	animate_nav('learn-more', 'background');
+
+	$("#div-hide").hide();
+	toggle_extras();
+
 });
